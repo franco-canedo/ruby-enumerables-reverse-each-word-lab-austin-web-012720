e04adfaedc140b3851cc752d5e7e6f505da1c65c
Franco Canedo <francanedo1@hotmail.com>
2020-01-13 19:51:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  
+  new_array = []
   reverse_array = []
-  new_array = %w[string]
+  new_array = string.join(" ")
   reverse_array = new_array.collect do |word|
     word.reverse 
   end
