bfb74ab937219ed89ed6c25a03ee583c1557c6aa
Franco Canedo <francanedo1@hotmail.com>
2020-01-13 19:47:14 +0000

    Automatically backed up by Learn

--- lib/reverse_each_word.rb
+++ lib/reverse_each_word.rb
@@ -1,7 +1,6 @@
-def reverse_each_word(string)
-  new_array = %w[string]
-  new_array.collect do |word|
-    word.reverse 
-  end
-    
-end 
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
