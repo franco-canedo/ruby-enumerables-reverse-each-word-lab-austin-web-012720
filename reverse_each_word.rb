e8acfe1eb57d0301e3056c643f0b89c928b05553
Franco Canedo <francanedo1@hotmail.com>
2020-01-13 19:54:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  new_array = []
+  reverse_array = []
+  new_array = string.split(" ")
+  reverse_array = new_array.collect do |word|
+    word.reverse 
+  end
+  reverse_array.join(" ")
+end 
